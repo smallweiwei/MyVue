f81f0c67a75a96c4ff71a13e7749863301b13b2b
PC-20160203LFTY\Administrator <1229046791@qq.com>
2018-02-19 14:27:18 +0800

    no message

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/Login'
+import Index from '@/components/Index'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: Index
+    },
+    {
+      path:'/login',
+      name:'login',
+      component: Login
+}
+  ]
+})
